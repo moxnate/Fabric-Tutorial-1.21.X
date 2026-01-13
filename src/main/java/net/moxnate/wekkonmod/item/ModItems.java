42dff8f24954470f0a046f9e35c7e7aae7434b92
moxnate <w8w87wnnv7@privaterelay.appleid.com>
2026-01-13 22:08:43 +0300

    add this 13.01.2026

---
+++ src/main/java/net/moxnate/wekkonmod/item/ModItems.java
@@ -0,0 +1,10 @@
+package net.moxnate.wekkonmod.item;
+
+import net.moxnate.wekkonmod.WekkonMod;
+
+public class ModItems {
+
+    public static void registerModItems() {
+        WekkonMod.LOGGER.info("Registering Mod Items for" + WekkonMod.MOD_ID);
+    }
+}
