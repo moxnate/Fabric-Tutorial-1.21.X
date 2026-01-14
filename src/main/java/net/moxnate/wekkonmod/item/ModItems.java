66c9d139ac956045ceddd6ccd2503f4100720687
moxnate <w8w87wnnv7@privaterelay.appleid.com>
2026-01-14 16:30:13 +0300

    added this 14.01.2026

--- src/main/java/net/moxnate/wekkonmod/item/ModItems.java
+++ src/main/java/net/moxnate/wekkonmod/item/ModItems.java
@@ -1,10 +1,28 @@
 package net.moxnate.wekkonmod.item;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 import net.moxnate.wekkonmod.WekkonMod;
 
 public class ModItems {
 
+    public static final Item PINK_PROXIUM = registerItem("pink_proxium", new Item(new Item.Settings()));
+    public static final Item RAW_PINK_PROXIUM = registerItem("raw_pink_proxium", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(WekkonMod.MOD_ID, name), item);
+    }
+
     public static void registerModItems() {
         WekkonMod.LOGGER.info("Registering Mod Items for" + WekkonMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(PINK_PROXIUM);
+            entries.add(RAW_PINK_PROXIUM);
+        });
     }
 }
