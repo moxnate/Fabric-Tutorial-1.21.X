34f32e7410303163ae2006b5e392d1dc07e366ec
moxnate <w8w87wnnv7@privaterelay.appleid.com>
2026-01-18 21:32:26 +0300

    added custom item tag for Magic Block added this 18.01.2026 21:32

--- src/main/java/net/moxnate/wekkonmod/block/custom/MagicBlock.java
+++ src/main/java/net/moxnate/wekkonmod/block/custom/MagicBlock.java
@@ -18,6 +18,7 @@ import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.moxnate.wekkonmod.item.ModItems;
+import net.moxnate.wekkonmod.util.ModTags;
 
 import java.util.List;
 
@@ -37,12 +38,16 @@ public class MagicBlock extends Block {
     @Override
     public void onSteppedOn(World world, BlockPos pos, BlockState state, Entity entity) {
         if(entity instanceof ItemEntity itemEntity) {
-            if(itemEntity.getStack().getItem() == ModItems.PURE_PROXIUM) {
+            if(isValidItem(itemEntity.getStack())) {
                 itemEntity.setStack(new ItemStack(Items.NETHERITE_INGOT, itemEntity.getStack().getCount()));
             }
         }
 
         super.onSteppedOn(world, pos, state, entity);
+    }
+
+    private boolean isValidItem(ItemStack stack) {
+        return stack.isIn(ModTags.Items.TRANSFORMABLE_ITEMS);
     }
 
     @Override
